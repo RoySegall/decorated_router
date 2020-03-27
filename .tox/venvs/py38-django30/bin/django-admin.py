3721d68b729a36ed2c04d000ba22203c009c67fb
RoySegall <roy@segall.io>
2020-03-27 12:20:35 +0300

    Now?

---
+++ .tox/venvs/py38-django30/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Applications/MAMP/htdocs/djangoTesting/django-decorated-router/.tox/venvs/py38-django30/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
