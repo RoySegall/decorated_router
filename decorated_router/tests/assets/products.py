fb0e7e5c30e0bc454579013df336a354a3150b66
Roy Segall <roy@segall.io>
2020-03-27 16:58:34 +0300

    Fix master.

--- decorated_router/tests/assets/products.py
+++ decorated_router/tests/assets/products.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.views import View
 
+
 class ProductsController(View):
     def get(self, request, show_title):
         return JsonResponse({'blogs': [
--- manage.py
+++ manage.py
@@ -4,7 +4,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "decorated_router.tests.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "decorated_router.tests.settings"
+    )
 
     from django.core.management import execute_from_command_line
 
